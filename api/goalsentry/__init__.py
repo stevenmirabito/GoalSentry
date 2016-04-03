6ed1d8756d6a26b514e9c6ac6a06f0276ec50826
Steven Mirabito <steven@stevenmirabito.com>
2016-04-03 01:49:14 -0400

    Flask dev server support

--- api/goalsentry/__init__.py
+++ api/goalsentry/__init__.py
@@ -1,8 +1,18 @@
-from flask import Flask, request
+from flask import Flask
 from database import session as db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "<h1 style='color:red'>Goal Sentry API</h1>"
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.remove()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
